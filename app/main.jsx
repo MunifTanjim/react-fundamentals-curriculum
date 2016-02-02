0fdb1cc1b186fe96f293b78e8feb0387c4b67147
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-02-02 03:11:25 -0700

    complete step 1

---
+++ app/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const HelloWorld = () => (
+  <span>
+    Hello World
+  </span>
+)
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+})
+
+const config = {
+  entry: [
+    "./app/main.jsx"
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      include: __dirname + '/app',
+      loader: 'babel'
+    }]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
+};
+
+module.exports = config;
